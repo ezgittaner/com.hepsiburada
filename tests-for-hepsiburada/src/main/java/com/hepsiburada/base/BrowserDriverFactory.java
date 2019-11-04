fedf75ef775556f34294dad3d5daed640e428441
Ezgi Taner <ezgittaner@gmail.com>
2019-11-04 19:32:10 +0300

    hepsiburada.com new test project environment

---
+++ tests-for-hepsiburada/src/main/java/com/hepsiburada/base/BrowserDriverFactory.java
@@ -0,0 +1,78 @@
+package com.hepsiburada.base;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class BrowserDriverFactory {
+
+	private ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
+	private String browser;
+	private Logger log;
+
+	public BrowserDriverFactory(String browser, Logger log) {
+		this.browser = browser.toLowerCase();
+		this.log = log;
+	}
+
+	public WebDriver createDriver() {
+		// Create driver
+		log.info("Create driver: " + browser);
+
+		switch (browser) {
+		case "chrome":
+			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+			driver.set(new ChromeDriver());
+			break;
+
+		case "firefox":
+			System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
+			driver.set(new FirefoxDriver());
+			break;
+
+		case "chromeheadless":
+			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+			ChromeOptions chromeOptions = new ChromeOptions();
+			chromeOptions.addArguments("--headless");
+			driver.set(new ChromeDriver(chromeOptions));
+			break;
+
+		case "firefoxheadless":
+			System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
+			FirefoxBinary firefoxBinary = new FirefoxBinary();
+			firefoxBinary.addCommandLineOptions("--headless");
+			FirefoxOptions firefoxOptions = new FirefoxOptions();
+			firefoxOptions.setBinary(firefoxBinary);
+			driver.set(new FirefoxDriver(firefoxOptions));
+			break;
+
+		default:
+			System.out.println("Do not know how to start: " + browser + ", starting chrome.");
+			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+			driver.set(new ChromeDriver());
+			break;
+		}
+
+		return driver.get();
+	}
+
+	public WebDriver createChromeWithMobileEmulation(String deviceName) {
+		log.info("Starting driver with " + deviceName + " emulation]");
+		Map<String, String> mobileEmulation = new HashMap<>();
+		mobileEmulation.put("deviceName", deviceName);
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
+
+		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+		driver.set(new ChromeDriver(chromeOptions));
+		return driver.get();
+	}
+
+}
